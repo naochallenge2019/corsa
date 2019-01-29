d6c88e4de964e51a2d2bf01350ea3a579cffe64f
leonardozolli <zolli.leonardo@gmail.com>
2019-01-29 15:05:59 +0100

    Add files via upload

---
+++ camminata_1.ino
@@ -0,0 +1,42 @@
+const int echo_1 = 2;
+const int trig_1 = 3;
+const int echo_2 = 6;
+const int trig_2 = 7;
+
+void setup() {
+
+  Serial.begin(9600);
+  pinMode(echo_1, INPUT);
+  pinMode(trig_1, OUTPUT);
+  pinMode(echo_2, INPUT);
+  pinMode(trig_2, OUTPUT);
+}
+
+void loop(){
+  
+  digitalWrite(trig_1, LOW);
+  //digitalWrite(trig_2, LOW);
+  digitalWrite(trig_1, HIGH);
+  //digitalWrite(trig_2, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig_1, LOW);
+  //digitalWrite(trig_2, LOW);
+  long durata_1=pulseIn(echo_1, HIGH);
+  //long durata_2=pulseIn(echo_2, HIGH);
+  long distanza_1=0.034*durata_1/2;  
+  //long distanza_2=0.034*durata_2/2;
+  Serial.print("sensore 1: ");
+  Serial.println(distanza_1);
+  
+  digitalWrite(trig_2, LOW);
+  digitalWrite(trig_2, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig_2, LOW);
+  long durata_2=pulseIn(echo_2, HIGH);
+  long distanza_2=0.034*durata_2/2;
+  Serial.print("sensore 2: ");
+  Serial.println(distanza_2);
+  delay(500);
+  
+  
+}
